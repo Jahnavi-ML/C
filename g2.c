4e92d2ef3a895cc963de4a852aa0423c5f03b306
Jahnavi-ML <m.lakshmijahnavi2001@gmail.com>
2021-07-26 21:37:03 +0530

    Gratter than of 2 numbers program done.

---
+++ g2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+ int a,b;
+ printf("enter a value:- ");
+ scanf("%d",&a);
+ printf("enter b value:- ");
+ scanf("%d",&b);
+ printf("%d",a>b?a:b);
+return 0;
+}
