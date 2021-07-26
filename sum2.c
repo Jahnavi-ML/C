d71a298650dffaffbb09bc9ccf4348256405db0a
Jahnavi-ML <m.lakshmijahnavi2001@gmail.com>
2021-07-26 21:00:29 +0530

    sum of 2 number program added

---
+++ sum2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+	int a=0,b=0;
+	printf("Enter a values:- ");
+	scanf("%d",&a);
+	printf("Enter b values:- ");
+	scanf("%d",&b);
+	printf("sum = %d",a+b);
+	return 0;
+}
